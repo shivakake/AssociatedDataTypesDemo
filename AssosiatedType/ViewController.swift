27e49c67513e4b3cffdba2557f14ffa6cdf5e7b0
Shiva Kumar <shiva.kake@gmail.com>
2023-08-09 16:03:48 +0530

    First Commit on 9th August 2023

--- AssosiatedType/ViewController.swift
+++ AssosiatedType/ViewController.swift
@@ -6,14 +6,51 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+protocol AssociateTypeDemoProtocol {
+    associatedtype DataType // here you can take anythink as datatype
+    // associatedtype as like generic datatypes
+    var colletion:[DataType] { get set }
+    func appendItemToCollection(item: DataType)
 }
 
+class ViewController: UIViewController {
+    
+    var colletion: [DataType] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+extension ViewController : AssociateTypeDemoProtocol {
+    
+    typealias DataType = String
+    
+    func appendItemToCollection(item: String) {
+        colletion.append(item)
+    }
+}
+
+class Car: AssociateTypeDemoProtocol {
+    
+    typealias DataType = Int
+    
+    var colletion: [Int] = []
+    
+    func appendItemToCollection(item: Int) {
+        colletion.append(item)
+    }
+}
+
+class Bus: AssociateTypeDemoProtocol {
+    
+    typealias DataType = Double
+    
+    var colletion: [Double] = []
+    
+    func appendItemToCollection(item: Double) {
+        colletion.append(item)
+    }
+}
